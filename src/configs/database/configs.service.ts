73a773a7e8cff7770640a42db480ddf8ebe3421a
eunkangchoi <ekk12mv2@gmail.com>
2022-11-04 21:29:55 +0900

    :wrench: config: Mysql 데이터베이스 커넥션 연결

---
+++ src/configs/database/configs.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from '@nestjs/typeorm';
+
+@Injectable()
+export class MySqlConfigService implements TypeOrmOptionsFactory {
+  constructor(private configService: ConfigService) {}
+  createTypeOrmOptions(): TypeOrmModuleOptions {
+    return {
+      type: 'mysql',
+      host: this.configService.get<string>('DB_HOST'),
+      port: +this.configService.get<number>('DB_PORT'),
+      username: this.configService.get<string>('DB_USERNAME'),
+      password: this.configService.get<string>('DB_PASSWORD'),
+      database: this.configService.get<string>('DB_DATABASE'),
+      entities: [__dirname + '../../../entities/*.{ts,js}'],
+      migrations: [__dirname + '../../../migrations/*.ts'],
+      charset: 'utf8mb4',
+      synchronize: true,
+      logging: true,
+      keepConnectionAlive: true,
+    };
+  }
+}
