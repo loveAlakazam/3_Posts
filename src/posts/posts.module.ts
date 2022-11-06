5ee5fcefdba6f7496230e8ec0810e75d25c43565
eunkangchoi <ekk12mv2@gmail.com>
2022-11-07 04:12:00 +0900

    fix: 모듈세팅

--- src/posts/posts.module.ts
+++ src/posts/posts.module.ts
@@ -3,9 +3,10 @@ import { PostsService } from './posts.service';
 import { PostsController } from './posts.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Posts } from '../entities/Posts';
+import { Users } from 'src/entities/Users';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Posts])],
+  imports: [TypeOrmModule.forFeature([Posts, Users])],
   controllers: [PostsController],
   providers: [PostsService],
 })
