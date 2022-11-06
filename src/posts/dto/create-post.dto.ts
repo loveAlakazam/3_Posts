6330f4776dcf9a58595d5992f9e0edc2fec322f6
eunkangchoi <ekk12mv2@gmail.com>
2022-11-06 18:33:32 +0900

    :art: chore: 게시물등록 dto 추가

--- src/posts/dto/create-post.dto.ts
+++ src/posts/dto/create-post.dto.ts
@@ -1,1 +1,28 @@
-export class CreatePostDto {}
+import {
+  IsEnum,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  MaxLength,
+} from 'class-validator';
+import { PostType } from '../../entities/enums/PostType';
+
+export class CreatePostDto {
+  @IsNotEmpty()
+  @IsString()
+  @MaxLength(20)
+  title: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @MaxLength(200)
+  content: string;
+
+  @IsNotEmpty()
+  @IsEnum(PostType)
+  postType: PostType;
+
+  @IsOptional()
+  @IsString()
+  postPassword?: string;
+}
