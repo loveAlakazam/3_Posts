008811e4944c8c9411551c6c02f2e144fa019f46
eunkangchoi <ekk12mv2@gmail.com>
2022-11-06 18:29:03 +0900

    :clown_face: test: 유닛테스트 환경세팅

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,10 +8,22 @@ import { MySqlConfigModule } from './configs/database/configs.module';
 import { MySqlConfigService } from './configs/database/configs.service';
 import { LoggerMiddleware } from './common/middlewares/loggers.middleware';
 import { PostsModule } from './posts/posts.module';
+import * as Joi from 'joi';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true,
+      validationSchema: Joi.object({
+        NODE_PORT: Joi.string().required(),
+        MYSQL_HOST: Joi.string().required(),
+        MYSQL_PORT: Joi.string().required(),
+        MYSQL_USERNAME: Joi.string().required(),
+        MYSQL_PASSWORD: Joi.string().required(),
+        MYSQL_DATABASE: Joi.string().required(),
+      }),
+    }),
     TypeOrmModule.forRootAsync({
       imports: [MySqlConfigModule],
       useClass: MySqlConfigService,
