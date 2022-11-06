853035d766073893868481f7bfc7e855da57d79a
eunkangchoi <ekk12mv2@gmail.com>
2022-11-07 04:29:45 +0900

    fix: 게시글 수정시 - 최소글자수 설정

--- src/posts/dto/update-post.dto.ts
+++ src/posts/dto/update-post.dto.ts
@@ -1,14 +1,16 @@
 import { Optional } from '@nestjs/common';
-import { IsOptional, IsString, MaxLength } from 'class-validator';
+import { IsOptional, IsString, MaxLength, MinLength } from 'class-validator';
 
 export class UpdatePostDto {
   @Optional()
   @IsString()
+  @MinLength(1)
   @MaxLength(20)
   title: string;
 
   @Optional()
   @IsString()
+  @MinLength(1)
   @MaxLength(200)
   content: string;
 
