f2fbfa38c117211558ad6a861fef7423711bb166
eunkangchoi <ekk12mv2@gmail.com>
2022-11-07 04:11:21 +0900

    feat: 게시글 컨트롤러 로직 작성

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -6,8 +6,10 @@ import {
   Patch,
   Param,
   Delete,
-  UseFilters,
   UseGuards,
+  Query,
+  ParseIntPipe,
+  UseFilters,
 } from '@nestjs/common';
 import { PostsService } from './posts.service';
 import { CreatePostDto } from './dto/create-post.dto';
@@ -15,36 +17,73 @@ import { UpdatePostDto } from './dto/update-post.dto';
 import { AuthenticatedGuard } from '../auth/auth.guard';
 import { Users } from '../entities/Users';
 import { User } from '../auth/auth.decorator';
+import { PostPagination } from './dto/post-pagination.dto';
+import { RemovePostDto } from './dto/remove-post.dto';
+import { HttpExceptionFilter } from 'src/common/utils/http-exception/http-exception.filter';
+import { PrivatePostDetailDto } from './dto/private-post-detail.dto';
 
 @Controller('posts')
 export class PostsController {
   constructor(private readonly postsService: PostsService) {}
 
+  /**
+   * [POST] /api/posts/
+   */
   @UseGuards(AuthenticatedGuard)
   @Post()
-  async create(@User() user: Users, @Body() createPostDto: CreatePostDto) {
+  async createPost(@User() user: Users, @Body() createPostDto: CreatePostDto) {
     return await this.postsService.createPost(user, createPostDto);
   }
 
+  /**
+   * [PATCH] /api/posts/:postId
+   */
+  @UseGuards(AuthenticatedGuard)
+  @Patch(':postId')
+  async updatePost(
+    @User() user: Users,
+    @Param('postId', ParseIntPipe) postId: number,
+    @Body() updatePostDto: UpdatePostDto,
+  ) {
+    return await this.postsService.updatePost(user, postId, updatePostDto);
+  }
+
+  /**
+   * [DELETE] /api/posts/:postId
+   */
+  @UseGuards(AuthenticatedGuard)
+  @Delete(':postId')
+  async removePost(
+    @User() user: Users,
+    @Param('postId', ParseIntPipe) postId: number,
+    @Body() removePostDto: RemovePostDto,
+  ) {
+    return await this.postsService.removePost(user, postId, removePostDto);
+  }
+
+  /**
+   * [GET] /api/posts/
+   *
+   * 모든 사용자는 헌 페이지 내에서 모든 게시글을 최신글 순서로 확인할 수 있다.
+   * 한 페이지에는 최대 20개의 게시물을 볼 수 있다.
+   * @returns
+   */
   @Get()
-  async findAll() {
-    return this.postsService.findAllPosts();
+  async findAll(@Query() postPagination: PostPagination) {
+    return await this.postsService.findAllPosts(postPagination);
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.postsService.findOnePost(+id);
-  }
-
-  @UseGuards(AuthenticatedGuard)
-  @Patch(':id')
-  updatePost(@Param('id') id: string, @Body() updatePostDto: UpdatePostDto) {
-    return this.postsService.updatePost(+id, updatePostDto);
-  }
-
-  @UseGuards(AuthenticatedGuard)
-  @Delete(':id')
-  removePost(@Param('id') id: string) {
-    return this.postsService.removePost(+id);
+  /**
+   * [GET] /api/posts/:id
+   *
+   * @param
+   * @returns
+   */
+  @Get(':postId')
+  async findOne(
+    @Param('postId', ParseIntPipe) postId: number,
+    @Body() privatePostDetailDto: PrivatePostDetailDto,
+  ) {
+    return await this.postsService.findOnePost(postId, privatePostDetailDto);
   }
 }
