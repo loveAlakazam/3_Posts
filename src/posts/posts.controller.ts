9b632e7cc98a33d02fe5ddb63c02c4bcbc7c64fa
eunkangchoi <ekk12mv2@gmail.com>
2022-11-06 18:26:00 +0900

    :pencil2: chore: 함수명 변경, guard추가

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,34 +1,50 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  UseFilters,
+  UseGuards,
+} from '@nestjs/common';
 import { PostsService } from './posts.service';
 import { CreatePostDto } from './dto/create-post.dto';
 import { UpdatePostDto } from './dto/update-post.dto';
+import { AuthenticatedGuard } from '../auth/auth.guard';
+import { Users } from '../entities/Users';
+import { User } from '../auth/auth.decorator';
 
 @Controller('posts')
 export class PostsController {
   constructor(private readonly postsService: PostsService) {}
 
+  @UseGuards(AuthenticatedGuard)
   @Post()
-  create(@Body() createPostDto: CreatePostDto) {
-    return this.postsService.create(createPostDto);
+  async create(@User() user: Users, @Body() createPostDto: CreatePostDto) {
+    return await this.postsService.createPost(user, createPostDto);
   }
 
   @Get()
-  findAll() {
-    return this.postsService.findAll();
+  async findAll() {
+    return this.postsService.findAllPosts();
   }
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return this.postsService.findOne(+id);
+    return this.postsService.findOnePost(+id);
   }
 
+  @UseGuards(AuthenticatedGuard)
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updatePostDto: UpdatePostDto) {
-    return this.postsService.update(+id, updatePostDto);
+  updatePost(@Param('id') id: string, @Body() updatePostDto: UpdatePostDto) {
+    return this.postsService.updatePost(+id, updatePostDto);
   }
 
+  @UseGuards(AuthenticatedGuard)
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.postsService.remove(+id);
+  removePost(@Param('id') id: string) {
+    return this.postsService.removePost(+id);
   }
 }
