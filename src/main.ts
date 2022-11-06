b25d4cb5ffc81962abda5209092c4ac8293a6e90
eunkangchoi <ekk12mv2@gmail.com>
2022-11-06 18:35:27 +0900

    :adhesive_bandage: chore: main 함수 수정
    cors 추가 & globalPipe 옵션 추가

--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,25 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { sessionConfig } from './auth/auth.session.config';
+import { HttpExceptionFilter } from './common/utils/http-exception/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('/api');
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+    }),
+  );
 
   sessionConfig(app);
 
-  const port = process.env.SERVER_PORT || 3000;
-  await app.listen(port);
+  // global middleware 설정 : cors 속성 활성화
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+  });
+  await app.listen(3000);
 }
 bootstrap();
