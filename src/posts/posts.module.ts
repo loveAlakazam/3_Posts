761dab1ea8f640fcf9d48ba08df13d6d232bfed8
loveAlakazam <dmsrkd1216@gmail.com>
2022-11-20 21:22:04 +0900

    :wrench: feat: repository 레이어 추가로 모듈변경

--- src/posts/posts.module.ts
+++ src/posts/posts.module.ts
@@ -3,11 +3,12 @@ import { PostsService } from './posts.service';
 import { PostsController } from './posts.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Posts } from '../entities/Posts';
-import { Users } from 'src/entities/Users';
+import { Users } from '../entities/Users';
+import { PostsRepository } from './posts.repository';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Posts, Users])],
   controllers: [PostsController],
-  providers: [PostsService],
+  providers: [PostsService, PostsRepository],
 })
 export class PostsModule {}
