7c77a48f46d25d2a3ff0703d414188f9fd74e622
loveAlakazam <dmsrkd1216@gmail.com>
2022-11-23 00:40:46 +0900

    :art: chore: 커스텀에러 리팩토링

--- src/posts/exceptions/error-messages.ts
+++ src/posts/exceptions/error-messages.ts
@@ -1,6 +1,16 @@
 export const errorMsg = {
   PRIVATE_POST_EMPTY_PASSWORD: '비밀글의 비밀번호가 존재하지 않습니다.',
-  PRIVATE_POST_INVALID_PASSWORD: '비밀글의 비밀번호가 일치하지 않습니다.',
-  INVALID_POST_PASSWORD:
+  INVALID_PASSWORD: '비밀글의 비밀번호가 일치하지 않습니다.',
+  INVALID_POST_PASSWORD_REGEX:
     '비밀번호는 6자이상이며 최소 한개의 숫자가 있어야합니다!',
+
+  FAIL_GET_WEATHER_DATA:
+    'Weather API로부터 현재날씨 데이터를 가져오는데 실패하였습니다.',
+
+  POST_TYPE_INVALID:
+    '입력하신 게시글 타입이 존재하지 않습니다. (공개글/비밀글 만 가능합니다.)',
+
+  EMPTY_PASSWORD: '비밀번호를 입력해주세요',
+  NOT_FOUND_POST: '존재하지 않은 게시글 읍니다.',
+  NOT_ALLOW_USER: '접근권한이 없습니다.',
 };
